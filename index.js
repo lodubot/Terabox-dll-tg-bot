f75a088fc55e34098a9a20f32f93457af857d9f8
lodubot <jaihacku@gmail.com>
2025-07-26 23:42:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.listen(PORT, () => {
 });
 
 // ✅ Fixed Bot Token
-const BOT_TOKEN = '86031144761:AAF6Pt7cylensV3646464644664';
+const BOT_TOKEN = '7772786423:AAFF5hb0i7w_NVJ_GNN5Jah5ARJpkcUPQBo';
 const bot = new Telegraf(BOT_TOKEN);
 
 // ✅ TeraBox URL Validation  
 const teraboxUrlRegex = /^https:\/\/(terabox\.com|1024terabox\.com|teraboxapp\.com|teraboxlink\.com|terasharelink\.com|terafileshare\.com)\/s\/[A-Za-z0-9-_]+$/;
 
 // ✅ Your Telegram Channel ID  
-const CHANNEL_ID = "-1008906645565465"; // 🔹 এখানে আপনার চ্যানেলের আইডি বসান  
+const CHANNEL_ID = "-1002715754153"; // 🔹 এখানে আপনার চ্যানেলের আইডি বসান  
 
 // ✅ /start Command  
 bot.start((ctx) => {
